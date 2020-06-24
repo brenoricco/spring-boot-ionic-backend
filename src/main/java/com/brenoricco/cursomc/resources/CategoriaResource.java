184bf7b7b264f2a34245060a9a7a6f134677418a
Breno Ricco <brenoriccodev@gmail.com>
2020-06-24 14:07:48 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/brenoricco/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/brenoricco/cursomc/resources/CategoriaResource.java
@@ -1,5 +1,7 @@
 package com.brenoricco.cursomc.resources;
 
+import java.net.URI;
+
 import com.brenoricco.cursomc.domains.Categoria;
 import com.brenoricco.cursomc.services.CategoriaService;
 
@@ -7,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -21,5 +26,13 @@ public class CategoriaResource {
     public ResponseEntity<?> find(@PathVariable Integer id) {
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/brenoricco/cursomc/services/CategoriaService.java
+++ src/main/java/com/brenoricco/cursomc/services/CategoriaService.java
@@ -17,9 +17,13 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElseThrow(() ->
-            new ObjectNotFoundException("Objeto não encontrado! Id: " + id +
-                                        ", Tipo: " + Categoria.class.getName()));
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+
+    public Categoria insert(Categoria obj) {
+        obj.setId(null);
+        return repo.save(obj);
     }
 
 }

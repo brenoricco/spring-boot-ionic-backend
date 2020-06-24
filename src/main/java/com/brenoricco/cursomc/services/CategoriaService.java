9635a72178f29e84d6e84fbbb6865472e3a2bbcb
Breno Ricco <brenoriccodev@gmail.com>
2020-06-24 17:20:30 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/brenoricco/cursomc/services/CategoriaService.java
+++ src/main/java/com/brenoricco/cursomc/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.brenoricco.cursomc.domains.Categoria;
+import com.brenoricco.cursomc.dto.CategoriaDTO;
 import com.brenoricco.cursomc.repositories.CategoriaRepository;
 import com.brenoricco.cursomc.services.exceptions.DataIntegrityException;
 import com.brenoricco.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
     public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
         return repo.findAll(pageRequest);
+    }
+
+    public Categoria fromDTO(CategoriaDTO objDto) {
+        return new Categoria(objDto.getId(), objDto.getNome());
     }
 }

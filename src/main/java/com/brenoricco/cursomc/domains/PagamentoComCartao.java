758ca66b3dfe003ce14931e0d3cce186bc4e5714
Breno Ricco <brenoriccodev@gmail.com>
2020-08-18 20:01:46 -0300

    Inserindo Pedido

--- src/main/java/com/brenoricco/cursomc/domains/PagamentoComCartao.java
+++ src/main/java/com/brenoricco/cursomc/domains/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.brenoricco.cursomc.domains;
 import javax.persistence.Entity;
 
 import com.brenoricco.cursomc.domains.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 
     private static final long serialVersionUID = 1L;
